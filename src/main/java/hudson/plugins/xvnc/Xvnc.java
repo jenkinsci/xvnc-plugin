93ae94be9e73e40d51fca972a8a5be32485be223
jglick <jglick@dev.java.net>
2008-10-15 21:51:23 +0000

    May need to: killall Xrealvnc

--- src/main/java/hudson/plugins/xvnc/Xvnc.java
+++ src/main/java/hudson/plugins/xvnc/Xvnc.java
@@ -71,7 +71,7 @@ public class Xvnc extends BuildWrapper {
                 // Do not release it; it may be "stuck" until cleaned up by an administrator.
                 //allocator.free(displayNumber);
                 throw new IOException("Failed to run '" + actualCmd + "' (exit code " + exit + "), blacklisting display #" + displayNumber +
-                                      "; consider adding to your Hudson launch script: killall Xvnc; rm -fv /tmp/.X*-lock /tmp/.X11-unix/X*");
+                                      "; consider adding to your Hudson launch script: killall Xvnc Xrealvnc; rm -fv /tmp/.X*-lock /tmp/.X11-unix/X*");
             }
         } else {
             vncserverCommand = null;
