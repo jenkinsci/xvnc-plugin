4f9088c7b1c80dcc83dfd8e6812864911dfaab03
Levon Saldamli <levon.sa@gmail.com>
2013-03-06 10:35:28 +0100

    Limit maximum display number to use

--- src/main/java/hudson/plugins/xvnc/DisplayAllocator.java
+++ src/main/java/hudson/plugins/xvnc/DisplayAllocator.java
@@ -10,19 +10,24 @@ import java.util.HashSet;
  * @author Kohsuke Kawaguchi
  */
 final class DisplayAllocator {
+    private static final int MAX_DISPLAY_NUMBER = 99;
     /**
      * Display numbers in use.
      */
     private final Set<Integer> numbers = new HashSet<Integer>();
 
+    private getRandomValue(final int min, final int max) {
+        int range = (max + 1) - min;
+        return min + (new Random().nextInt(range))
+    }
+
     public synchronized int allocate(int baseDisplayNumber) {
-        int i;
+        int number;
         do {
-            int randomIncrement = new Random().nextInt(10000);
-            i = baseDisplayNumber + randomIncrement;
-        } while(numbers.contains(i));
-        numbers.add(i);
-        return i;
+            number = baseDisplayNumber + ;
+        } while(numbers.contains(number));
+        numbers.add(number);
+        return number;
     }
 
     public synchronized void free(int n) {
